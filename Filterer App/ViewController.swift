168857d051f494780f6818bb0e082b9aafb39d79
Arpan Badeka <badeka@usc.edu>
2016-01-07 10:15:41 +0530

    Initial Commit

---
+++ Filterer App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filterer App
+//
+//  Created by Arpan Badeka on 07/01/16.
+//  Copyright © 2016 abc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
